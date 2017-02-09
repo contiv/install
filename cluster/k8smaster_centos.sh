9ba7aaf345964916fb1b02860023a4bedbefd5f0
Neelima Mukiri <neelimam@gmail.com>
2017-02-09 12:59:04 -0800

    Add service-cidr option to kubeadm install

--- cluster/k8smaster_centos.sh
+++ cluster/k8smaster_centos.sh
@@ -1,2 +1,2 @@
-kubeadm init --token=$1 --api-advertise-addresses=$2 --skip-preflight-checks=true --use-kubernetes-version $3
+kubeadm init --token=$1 --api-advertise-addresses=$2 --skip-preflight-checks=true --use-kubernetes-version $3 --service-cidr 10.254.0.0/16
 
