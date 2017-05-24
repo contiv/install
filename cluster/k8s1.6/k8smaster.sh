874639069436829d77fc10bb28d15fb989be8ad3
Neelima Mukiri <neelimam@gmail.com>
2017-05-23 23:33:15 -0700

    Fix permissions for k8s 1.6

--- cluster/k8s1.6/k8smaster.sh
+++ cluster/k8s1.6/k8smaster.sh
@@ -1,1 +1,1 @@
-kubeadm init --token=$1 --apiserver-advertise-address=$2 --skip-preflight-checks=true --kubernetes-version $3 --service-cidr 10.254.0.0/16
+kubeadm init --token=$1 --apiserver-advertise-address=$2 --skip-preflight-checks=true --kubernetes-version $3
