67ad55fc1cbf9af066566f0496c154f6d0dd6eae
Doug Jacobs <dojacobs@cisco.com>
2017-06-14 17:07:29 -0700

    Initial checkin.  Scripts to prep & install docker for Docker-Swarm + v2plugin.

---
+++ cluster/docker17/centos_prep.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+set -euo pipefail
+
+if [ $EUID -ne 0 ]; then
+	echo "Please run this script as root user"
+	exit 1
+fi
+
+setenforce 0
+
+yum install -y ntp
+systemctl enable ntpd && systemctl start ntpd
+
+if systemctl -q is-active firewalld; then
+	systemctl stop firewalld
+fi
+if systemctl -q is-enabled firewalld; then
+	systemctl disable firewalld
+fi
+
+exit 0
