6d5a519ce8217a8f9ea2822749967ba3dd4f40ad
Bill Robinson <dseevr@users.noreply.github.com>
2017-06-19 11:43:09 -0700

    scripts/get_latest_release.py: allow errors to go unhandled instead of returning a default version
    Signed-off-by: Bill Robinson <dseevr@users.noreply.github.com>

--- scripts/get_latest_release.py
+++ scripts/get_latest_release.py
@@ -4,29 +4,13 @@ import json
 import requests
 import sys
 
-DEFAULT_VERSION = "1.0.0"
-
 HTTP_OK = 200
 
 # Try to get the latest release version
 r = requests.get("https://api.github.com/repos/contiv/install/releases/latest")
 if r.status_code != HTTP_OK:
-   # Get all the releases (include pre-releases) and pick the first of them
-   r = requests.get("https://api.github.com/repos/contiv/install/releases")
-   if r.status_code != HTTP_OK:
-      # Return a known version
-      print DEFAULT_VERSION
-      sys.exit(1)
-
-   try:
-      releases = json.loads(r.content)
-      print releases[0]['name']
-   except:
-      print DEFAULT_VERSION
-      sys.exit(1)
-try:
+   print >> sys.stderr, "failed to get latest release, status code: %d" % r.status_code
+   sys.exit(1)
+else:
    releases = json.loads(r.content)
    print releases['name']
-except:
-    print DEFAULT_VERSION
-    sys.exit(1)
