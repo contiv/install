05ef56a5dcdafacca35187e41caa64030a1b6886
Wei Tie <wtie@cisco.com>
2017-12-14 12:38:15 -0800

    Update base on reviews
    Signed-off-by: Wei Tie <wtie@cisco.com>

--- scripts/prepare_netplugin_images.sh
+++ scripts/prepare_netplugin_images.sh
@@ -16,35 +16,33 @@ mkdir -p "$CONTIV_ARTIFACT_STAGING"
 if [ -z "${NETPLUGIN_BRANCH:-}" ]; then
 	echo "Trying to use dockerhub contiv/netplugin:${CONTIV_NETPLUGIN_VERSION}"
 	# ensure the image exists
-	http_rc=$(curl -s -w "%{http_code}" -o /dev/null https://hub.docker.com/v2/repositories/contiv/netplugin/tags/${CONTIV_NETPLUGIN_VERSION}/)
+	http_rc=$(curl -L -s -w "%{http_code}" -o /dev/null https://hub.docker.com/v2/repositories/contiv/netplugin/tags/${CONTIV_NETPLUGIN_VERSION}/)
 	if [ "$http_rc" = 200 ]; then
 		echo "Found contiv/netplugin:${CONTIV_NETPLUGIN_VERSION} on dockerhub, exit 0"
-		exit 0
-	elif [ "$http_rc" = 404 ]; then
-		echo "Failed to find contiv/netplugin:${CONTIV_NETPLUGIN_VERSION} on dockerhub, exit 1"
+	else
+		echo "Failed to find contiv/netplugin:${CONTIV_NETPLUGIN_VERSION} on dockerhub, return code $http_rc, exit 1"
 		exit 1
 	fi
+else
+	# tempdir for building and cleanup on exit
+	netplugin_tmp_dir="$(mktemp -d)"
+	trap 'rm -rf ${netplugin_tmp_dir}' EXIT
+
+	echo Cloning ${NETPLUGIN_OWNER}/netplugin branch ${NETPLUGIN_BRANCH}
+	# about 3x faster to pull the HEAD of a branch with no history
+	git clone --branch ${NETPLUGIN_BRANCH} --depth 1 \
+		https://github.com/${NETPLUGIN_OWNER}/netplugin.git \
+		${netplugin_tmp_dir}/netplugin
+
+	# Try to build docker image locally
+	cd $netplugin_tmp_dir/netplugin
+	make host-build-docker-image
+
+	# the new built image tagged contivbase:latest
+	# below codes probably should goto netplugin, here just a hacking way
+	docker tag contivbase:latest contiv/netplugin:${CONTIV_NETPLUGIN_VERSION}
+	docker save contiv/netplugin:${CONTIV_NETPLUGIN_VERSION} -o $CONTIV_ARTIFACT_STAGING/netplugin-image-${CONTIV_NETPLUGIN_VERSION}.tar
 fi
-
-# tempdir for building and cleanup on exit
-netplugin_tmp_dir="$(mktemp -d)"
-trap 'rm -rf ${netplugin_tmp_dir}' EXIT
-
-echo Cloning ${NETPLUGIN_OWNER}/netplugin branch ${NETPLUGIN_BRANCH}
-# about 3x faster to pull the HEAD of a branch with no history
-git clone --branch ${NETPLUGIN_BRANCH} --depth 1 \
-	https://github.com/${NETPLUGIN_OWNER}/netplugin.git \
-	${netplugin_tmp_dir}/netplugin
-
-# Try to build docker image locally
-cd $netplugin_tmp_dir/netplugin
-make host-build-docker-image
-
-# the new built image tagged contivbase:latest
-# below codes probably should goto netplugin, here just a hacking way
-docker tag contivbase:latest contiv/netplugin:${CONTIV_NETPLUGIN_VERSION}
-docker save contiv/netplugin:${CONTIV_NETPLUGIN_VERSION} -o $CONTIV_ARTIFACT_STAGING/netplugin-image-${CONTIV_NETPLUGIN_VERSION}.tar
-
 #### ENSURE contiv/auth_proxy:<tag> exists #####
 
 auth_proxy_version=${CONTIV_API_PROXY_VERSION:-$DEFAULT_DOWNLOAD_CONTIV_VERSION}
@@ -52,29 +50,28 @@ auth_proxy_version=${CONTIV_API_PROXY_VERSION:-$DEFAULT_DOWNLOAD_CONTIV_VERSION}
 if [ -z "${NETPLUGIN_AUTH_PROXY_BRANCH:-}" ]; then
 	echo "Trying to use dockerhub contiv/auth_proxy:${auth_proxy_version}"
 	# ensure the image exists
-	http_rc=$(curl -s -w "%{http_code}" -o /dev/null https://hub.docker.com/v2/repositories/contiv/auth_proxy/tags/${auth_proxy_version}/)
+	http_rc=$(curl -L -s -w "%{http_code}" -o /dev/null https://hub.docker.com/v2/repositories/contiv/auth_proxy/tags/${auth_proxy_version}/)
 	if [ "$http_rc" = 200 ]; then
 		echo "Found contiv/auth_proxy:${auth_proxy_version} on dockerhub, exit 0"
-		exit 0
-	elif [ "$http_rc" = 404 ]; then
-		echo "Failed to find contiv/auth_proxy:${auth_proxy_version} on dockerhub, exit 1"
+	else
+		echo "Failed to find contiv/auth_proxy:${auth_proxy_version} on dockerhub, return code $http_rc, exit 1"
 		exit 1
 	fi
+else
+	# tempdir for building and cleanup on exit
+	auth_proxy_tmp_dir="$(mktemp -d)"
+	trap 'rm -rf ${auth_proxy_tmp_dir}' EXIT
+
+	echo Cloning ${NETPLUGIN_AUTH_PROXY_OWNER}/auth_proxy branch ${NETPLUGIN_AUTH_PROXY_BRANCH}
+	# about 3x faster to pull the HEAD of a branch with no history
+	git clone --branch ${NETPLUGIN_AUTH_PROXY_BRANCH} --depth 1 \
+		https://github.com/${NETPLUGIN_AUTH_PROXY_OWNER}/auth_proxy.git \
+		${auth_proxy_tmp_dir}/auth_proxy
+
+	# Try to build docker image locally
+	cd $auth_proxy_tmp_dir/auth_proxy
+	# this also checkouts contiv-ui master branch
+	BUILD_VERSION=master make build
+	# tag the same version with netplugin
+	docker tag contiv/auth_proxy:master contiv/auth_proxy:${auth_proxy_version}
 fi
-
-# tempdir for building and cleanup on exit
-auth_proxy_tmp_dir="$(mktemp -d)"
-trap 'rm -rf ${auth_proxy_tmp_dir}' EXIT
-
-echo Cloning ${NETPLUGIN_AUTH_PROXY_OWNER}/auth_proxy branch ${NETPLUGIN_AUTH_PROXY_BRANCH}
-# about 3x faster to pull the HEAD of a branch with no history
-git clone --branch ${NETPLUGIN_AUTH_PROXY_BRANCH} --depth 1 \
-	https://github.com/${NETPLUGIN_AUTH_PROXY_OWNER}/auth_proxy.git \
-	${auth_proxy_tmp_dir}/auth_proxy
-
-# Try to build docker image locally
-cd $auth_proxy_tmp_dir/auth_proxy
-# this also checkouts contiv-ui master branch
-BUILD_VERSION=master make build
-# tag the same version with netplugin
-docker tag contiv/auth_proxy:master contiv/auth_proxy:${auth_proxy_version}
